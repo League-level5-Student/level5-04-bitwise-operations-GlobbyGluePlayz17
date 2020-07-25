a0532c99de0a356fe3d3faab3c63ddbd2da21909
GlobbyGluePlayz17 <dudisd888@gmail.com>
2020-07-25 15:57:04 -0700

    BinaryPrinter, Base64Decorder(ip)
    BinaryPrinter (but werid numbers)
    Base64Decoder (determine the relationship between base64 values and characters)

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -21,44 +21,43 @@ public class BinaryPrinter {
 	
 	public void printShortBinary(short s) {
 		System.out.println(s);
-		for (int i = 16; i <= 0; i++) {
-			int binary = 10^i;
-			binary = Integer.toBinaryString(binary);
-			System.out.print((s & binary) >> i);
-		}
-		System.out.print((s & 0b10000000) >> 8);
-		System.out.print((s & 0b10000000) >> 7);
-		System.out.print((s & 0b1000000) >> 6);
-		System.out.print((s & 0b100000) >> 5);
-		System.out.print((s & 0b10000) >> 4);
-		System.out.print((s & 0b1000) >> 3);
-		System.out.print((s & 0b100) >> 2);	
-		System.out.print((s & 0b10) >> 1);
-		System.out.print((s & 0b1) >> 0);
+		printByteBinary((byte)((s & 0xFF00) >> 8));
+		//System.out.println("\n");
+		printByteBinary((byte)((s & 0x00FF) >> 0));
 	}
 	
 	public void printIntBinary(int i) {
-		int binary[] = new int[Integer.BYTES];
-		int index = 0;
-		while (i > 0) {
-			binary[index++] = i%2;
-			i = (i/2);
-		}
+//		int binary[] = new int[Integer.BYTES];
+//		int index = 0;
+//		while (i > 0) {
+//			binary[index++] = i%2;
+//			i = (i/2);
+//		}
+//		System.out.println(i);
 		System.out.println(i);
+		printShortBinary((short)((i & 0xFFFF0000) >> 16));
+		//System.out.println("\n");
+		printShortBinary((short)((i & 0x0000FFFF) >> 0));
+		
 	}
 	
 	public void printLongBinary(long l) {
-		
+		System.out.println(l);
+		printIntBinary((int)(l >> 32));
+	    System.out.print( " " );
+		printIntBinary((int)((l & 0x00000000FFFFFFFF) >> 0));
+
 	}
 	
 	public static void main(String[] args) {
 		BinaryPrinter bp = new BinaryPrinter();
+		System.out.println("\n Byte");
 		bp.printByteBinary(Byte.MAX_VALUE);
-		System.out.println("\n -=-=+=-=-");
+		System.out.println("\n -=-=+=-=- \n Short");
 		bp.printShortBinary(Short.MAX_VALUE);
-		System.out.println("\n -=-=+=-=-");
+		System.out.println("\n -=-=+=-=- \n Int");
 		bp.printIntBinary(4);
-		System.out.println("\n -=-=+=-=-");
+		System.out.println("\n -=-=+=-=- \n Long");
 		bp.printLongBinary(Long.MAX_VALUE);
 		
 		
--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -37,13 +37,16 @@ public class Base64Decoder {
 	//1. Complete this method so that it returns the the element in
 	//   the base64Chars array that corresponds to the passed in char.
 	public static byte convertBase64Char(char c){
-		return 0;
+		return base64Chars[];
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
+		if (s.length() <= 4) {
+			
+		}
 		return null;
 	}
 	

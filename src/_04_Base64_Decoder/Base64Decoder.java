870514b1ca6973c0fd3f31d6248ec3f4355ed3e6
GlobbyGluePlayz17 <dudisd888@gmail.com>
2020-08-01 16:00:02 -0700

    Base64Decoder (ip)
    whaat
    
    -for second test of testBase64ToByteArray, bits is correct but spliting it and casting the 8 bits to byte get -57????

--- src/_04_Base64_Decoder/Base64Decoder.java
+++ src/_04_Base64_Decoder/Base64Decoder.java
@@ -3,6 +3,8 @@ package _04_Base64_Decoder;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
+import _00_Binary_Conversion.DecimalToBinary;
+
 public class Base64Decoder {
 	/*
 	 * Base 64 is a way of encoding binary data using text.
@@ -34,25 +36,55 @@ public class Base64Decoder {
 		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
 	};
 	
-	//1. Complete this method so that it returns the the element in
+	//1. Complete this method so that it returns the the element *index* in
 	//   the base64Chars array that corresponds to the passed in char.
-	public static byte convertBase64Char(char c){
-		return base64Chars[];
+	public static byte convertBase64Char(char c){  //char = int so 4 bytes
+		byte ib = 0;
+		for (int i = 0; i < base64Chars.length; i++) {
+			if (base64Chars[i] == c) {
+				ib = (byte) i;
+				break;
+			}
+		}
+		return ib;
 	}
 	
 	//2. Complete this method so that it will take in a string that is 4 
 	//   characters long and return an array of 3 bytes (24 bits). The byte 
 	//   array should be the binary value of the encoded characters.
 	public static byte[] convert4CharsTo24Bits(String s){
-		if (s.length() <= 4) {
-			
-		}
-		return null;
+		byte[] b = new byte[3];
+		String bits = "";
+		if (s.length() == 4) {
+			for (int i = 0; i < s.length(); i++) {
+				char c = s.charAt(i);
+				for (int j = 0; j < base64Chars.length; j++) {
+					if (base64Chars[j] == c) {
+						String temp = DecimalToBinary.convertDecimalToBinary(j);
+						while (temp.length() != 6) {
+							temp += "0";
+						}
+						System.out.println("new bits = " + temp);
+						bits += temp;
+					}
+				}
+			}
+			//**onlyprintone0,needthefulltogetalldigits
+			System.out.println("complete bits = " + bits);
+			for (int i = 0; i < 3; i++) {
+				//b[i] = (byte) Integer.parseInt(bits.substring(i*8, 8*(i+1)));
+				b[i] = (byte) 255; //w haaaaaaaaaaaaa
+				System.out.println(b[i]);
+			}
+			//System.out.println("bit length: " + bits.length());
+		} 
+		return b;
 	}
 	
 	//3. Complete this method so that it takes in a string of any length
 	//   and returns the full byte array of the decoded base64 characters.
 	public static byte[] base64StringToByteArray(String file) {
+		byte[] b = new byte[file.length()*6/8];
 		return null;
 	}
 }
